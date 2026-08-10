07bf75ca80f4e66d55d85e4b0010b0943486a3ce
Suraj Nandakumar Pal <149008391+Surajpal011@users.noreply.github.com>
2026-08-11 02:19:57 +0530

    Time: 38 ms (28.44%), Space: 58.4 MB (78.3%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    nums.sort((a, b) => a - b);
+    n = nums.length;
+    let option1 = nums[n - 1]*nums[n - 2]*nums[n-3];
+    let option2 = nums[0] * nums[1] * nums[n - 1];
+    result = Math.max(option1,option2);
+    return result;
+};
