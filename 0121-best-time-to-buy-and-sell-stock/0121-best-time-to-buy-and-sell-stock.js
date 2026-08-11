676d750a5b7ded4d8e0f37e7a39d8e13d0e31751
Suraj Nandakumar Pal <149008391+Surajpal011@users.noreply.github.com>
2026-08-11 11:54:21 +0530

    Time: 0 ms (100%), Space: 65.5 MB (32.53%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minSoFar = prices[0];
+    let res = 0;    
+    for (let i = 1; i < prices.length; i++) {
+        minSoFar = Math.min(minSoFar, prices[i]);
+        res = Math.max(res, prices[i] - minSoFar);
+    }
+    return res;
+};
